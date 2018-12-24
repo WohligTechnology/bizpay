13b6c2336fa57f6c9b40be83f53ed2c5e2239653
varunnagda95 <varun.nagda@wohlig.com>
2018-12-24 17:41:39 +0530

    bizpaye tabs

--- www/lib/ionic/js/ionic-angular.js
+++ www/lib/ionic/js/ionic-angular.js
@@ -14367,7 +14367,7 @@ function($timeout, $ionicConfig) {
  * either an individual view basis, or by global configuration. Please see the
  * _Caching_ section in {@link ionic.directive:ionNavView} for more info.
  *
- * @param {string=} view-title A text-only title to display on the parent {@link ionic.directive:ionNavBar}.
+ * @param {string=} view-title A text-only title to display on the parent {@link ionic.directive:ionNaionNavBarvBar}.
  * For an HTML title, such as an image, see {@link ionic.directive:ionNavTitle} instead.
  * @param {boolean=} cache-view If this view should be allowed to be cached or not.
  * Please see the _Caching_ section in {@link ionic.directive:ionNavView} for
