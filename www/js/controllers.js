f13a22daa1b46bd6305c3e620ae4a6972a99696a
sohan1 <shn619@gmail.com>
2018-12-28 17:37:26 +0530

    controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
-angular.module('starter.controllers', [])
+angular
+  .module("starter.controllers", [])
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
-
+  .controller("AppCtrl", function($scope, $ionicModal, $timeout) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -13,52 +13,53 @@ angular.module('starter.controllers', [])
     $scope.loginData = {};
 
     // Create the login modal that we will use later
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-      scope: $scope
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
+    $ionicModal
+      .fromTemplateUrl("templates/login.html", {
+        scope: $scope
+      })
+      .then(function(modal) {
+        $scope.modal = modal;
+      });
 
     // Triggered in the login modal to close it
-    $scope.closeLogin = function () {
+    $scope.closeLogin = function() {
       $scope.modal.hide();
     };
 
     // Open the login modal
-    $scope.login = function () {
+    $scope.login = function() {
       $scope.modal.show();
     };
 
     // Perform the login action when the user submits the login form
-    $scope.doLogin = function () {
-      console.log('Doing login', $scope.loginData);
+    $scope.doLogin = function() {
+      console.log("Doing login", $scope.loginData);
 
       // Simulate a login delay. Remove this and replace with your login
       // code if using a login system
-      $timeout(function () {
+      $timeout(function() {
         $scope.closeLogin();
       }, 1000);
     };
   })
 
+  .controller("HomeCtrl", function($scope) {
+    module.controller("HomeCtrl", function($cordovaInAppBrowser) {
+      var options = {
+        location: "yes",
+        clearcache: "yes",
+        toolbar: "no"
+      };
 
-  .controller('HomeCtrl', function ($scope) {
-      module.controller('HomeCtrl', function ($cordovaInAppBrowser) {
-
-        var options = {
-          location: 'yes',
-          clearcache: 'yes',
-          toolbar: 'no'
-        };
-
-        document.addEventListener("deviceready", function () {
-          $cordovaInAppBrowser.open('http://ngcordova.com', '_blank', options)
-            .then(function (event) {
-              // success
-            })
-            .catch(function (event) {
-              // error
-            });
-        })
-      })
-    }
+      document.addEventListener("deviceready", function() {
+        $cordovaInAppBrowser
+          .open("http://ngcordova.com", "_blank", options)
+          .then(function(event) {
+            // success
+          })
+          .catch(function(event) {
+            // error
+          });
+      });
+    });
+  });
